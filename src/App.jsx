a93c912c928a69cf89bb05277c08c77cbeb1272c
ehachero <hachero@avaya.com>
2024-09-11 16:34:38 +0200

    subiendo react a vercel

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HelloWord from './components/HelloWord'
+
+const App = () => {
+  return (
+    <div>
+    <HelloWord/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/HelloWord.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HelloWord = () => {
+  return (
+    <div>Hello Word</div>
+  )
+}
+
+export default HelloWord
